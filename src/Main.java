44053d5a8b7d21d66fb3b27eb3208f7552fb6005
richard483 <richard.william483@gmail.com>
2021-11-05 11:43:25 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,134 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public Main() {// ini wrapper class
+		// TODO Auto-generated constructor stub
+//		int angka = 2;
+//		long lebihBanyakAngka = 1000000;
+//		double angkaKoma = 2.1112233;
+//		
+//		char hashtag = '#';
+//		
+//		System.out.print(lebihBanyakAngka);
+//		System.out.println(lebihBanyakAngka);
+//		System.out.printf("%d ", angka);
+		
+		// contoh tipe data abstract Scanner
+		
+		Scanner scan = new Scanner(System.in);
+		
+//		int harga = 100000;
+//		
+//		System.out.println("Masukkan harga2:");
+//		int harga2 = scan.nextInt();
+//		
+//		System.out.println(harga);
+//		System.out.println(harga2);
+		
+		
+//		System.out.println("Masukkan Nama kamu:");
+//		String namaKamu = scan.nextLine();
+//		
+//		System.out.println("Masukkan umur kamu");
+//		int umur = scan.nextInt();
+//		
+//		System.out.print("Nama kamu adalah: ");
+//		System.out.println(namaKamu);
+//		
+//		System.out.print("Umur kamu adalah: ");
+//		System.out.println(umur);
+//		
+//		System.out.print("Umur kamu 5 tahun lalu adalah: ");
+//		System.out.println(umur - 5);
+		
+//		//operasi dasar
+//		
+//		System.out.println("5 kali 3 adalah: " + 5*3);
+//		int hasil = 7%4; //3
+//		hasil--; //hasil = hasil - 1
+//		
+//		System.out.println("hasil: " + hasil);
+		
+		//operasi bitwise
+		int a = 2;
+		int b = 2;
+//		System.out.println(a==b);
+//		System.out.println(a!=b);
+//		System.out.println(a>=b);
+//		System.out.println(a>b);
+//		System.out.println(a<b);
+		
+//		// || => or
+//		// && => and
+//		System.out.println((4%2 == 1) || (2>3));
+//		System.out.println(true && true);
+
+		//selection
+		// if-else
+//		int angka = 12;
+//		if(angka == 10) {
+//			System.out.println("Ini angka 10");
+//		}
+//		
+//		else if(angka == 11) {
+//			System.out.println("Ini angka 11");
+//		}
+//		
+//		else {
+//			System.out.println("Ini bukan angka 10 atau 11");
+//		}
+//		
+		
+		//switch
+//		int angka = 1;
+//		
+//		switch (angka) {
+//		case 1:
+//			System.out.println("ini 1");
+//			break;
+//		case 2:
+//			System.out.println("ini 2");
+//			break;
+//		default:
+//			break;
+//		}
+		
+		//ternary
+		//[argumen]?true:false;
+//		String hasil;
+//		hasil = (2>3)? "iya!" : "tidak!";
+//		System.out.println(hasil);
+		
+		//repetition
+		//while
+//		int n = 5;//4, 3, 2, 1, 0
+//		while(n > 0) {
+//			System.out.println(n);
+//			n--;
+//		}
+		
+		//do while
+		
+		int n = 10;
+//		do {
+//			System.out.println(n);
+//			n = n-1;
+//		}while(n > 0);
+		//for
+		for(int i=0; i<n; i++) {
+			System.out.println(i);
+		}
+		
+		
+		
+		
+	}
+
+	public static void main(String[] args) {// ini dimana kodingan bakal dibaca duluan
+		// TODO Auto-generated method stub
+		new Main();
+
+	}
+
+}
